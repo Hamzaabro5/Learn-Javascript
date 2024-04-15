bcc92edb8aa89cb53980845aee853c39231e1953
Hamzaabro5 <hamzaabro683@gmail.com>
2024-04-15 10:37:33 +0500

    Learning Javascript

---
+++ Class-16 (Revision)/app.js
@@ -0,0 +1,20 @@
+// const num1 = +prompt(`Give First Numbers`)
+// const num2 = +prompt(`Give Second Numbers`)
+
+// const totalNum = num1 + num2
+
+// if (totalNum > 100) {
+//     console.log(`Limit is reached`);
+// }else{
+//     console.log(totalNum);
+// }
+
+
+
+
+// const h1 = document.querySelector(`h1`)
+
+
+// function greetUser() {
+//     h1.innerHTML = `Hello JavaScript`
+// }
