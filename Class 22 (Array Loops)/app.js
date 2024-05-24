ad6aec74b6533a6a9af444f88722df4051612b4b
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-24 23:03:23 +0500

    learn JavaScript

---
+++ Class 22 (Array Loops)/app.js
@@ -0,0 +1,49 @@
+// forEach
+// forOf
+// forIn
+// map
+// filter
+
+const arr = [`apple` , `mango` , `orange` , `grapes` , `banana`];
+
+
+// // ForOf
+
+// for (const iterator of arr) {
+//     console.log(iterator);
+// }
+
+
+
+
+
+
+// // ForIn
+// const obj = {
+//     username: `hamza`,
+//     email: `hamzaabro574@gmail.com`,
+//     age: 18,
+// }
+
+// for (const key in obj) {
+//   console.log(key);
+// }
+
+
+
+
+
+// // ForEach
+
+// arr.forEach(function(items , index) {
+//     console.log(items);
+//     console.log(index);
+// })
+
+// // for each k ander jo function h wo callback function h jo as an argument dete hain
+
+
+
+
+
+
--- Class-20 (DOM = Document Object Model)/app.js
+++ Class-20 (DOM = Document Object Model)/app.js
@@ -33,3 +33,4 @@ function createList() {
 
     console.log(ul)
 }
+
