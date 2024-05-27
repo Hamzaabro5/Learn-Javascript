6b68d4eb7492ac414a2a95ef089d145f8afd1b42
Hamzaabro5 <hamzaabro683@gmail.com>
2024-05-27 10:53:27 +0500

    Learn JavaScript

--- Class 24 (Synchronous and Asynchronous)/app.js
+++ Class 24 (Synchronous and Asynchronous)/app.js
@@ -24,6 +24,7 @@
 // async await
 // axios
 
+// They All Work as Asynchronous
 
 
 
@@ -35,4 +36,70 @@
 
 
 
-// JavaScript is an asynchronous and concurrent programming language that offers a lot of flexibility. It's single-threaded like synchronous but also non-blocking like asynchronous. Although it's synchronous by nature, JavaScript benefits from an asynchronous process.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Synchronous:
+
+// Code execution happens in a sequence, one line at a time.
+// Each operation must complete before the next one starts.
+// If an operation is slow (e.g., reading a large file), it can block the entire program.
+
+
+
+
+
+
+
+
+
+// Asynchronous:
+
+// Operations can happen out of sequence.
+// Allows the program to continue executing other code while waiting for the asynchronous operation to complete.
+// Common in operations like I/O (input/output), where waiting for external resources (like a file read or an API call) would otherwise block the program.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this question is come in interviews so tell
+// JavaScript is an Synchronous language
